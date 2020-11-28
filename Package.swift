07c2059376b0b21f219af86a7b2454c397e3def2
Ivan Myrvold <ivan.myrvold@abax.no>
2020-11-28 16:38:51 +0100

    changed url to IvanPublishTheme to use https instead of git

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     dependencies: [
         .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
         .package(name: "SplashPublishPlugin", url: "https://github.com/johnsundell/splashpublishplugin", from: "0.1.0"),
-        .package(name: "IvanPublishTheme", url: "git@github.com:imyrvold/IvanPublishTheme.git", from: "1.0.0") 
+        .package(name: "IvanPublishTheme", url: "https://github.com/imyrvold/IvanPublishTheme.git", from: "1.0.0")
     ],
     targets: [
         .target(
